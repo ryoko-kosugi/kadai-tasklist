680435426e75805f11955298ef55e53a8afbd9b2
ryoko.kosugi <55273342+ryoko-kosugi@users.noreply.github.com>
2019-10-23 09:55:01 +0000

    user login

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,27 +8,12 @@
     </head>
 
     <body>
-        <header class="mb-4">
-            <nav class="navbar navbar-expand-sm navbar-dark bg-dark">
-                <a class="navbar-brand" href="/">TaskList</a>
-                
-                <button type="button" class="navbar-toggler" data-toggle="collapse" data-target="#nav-bar">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-                
-                <div class="collapse navbar-collapse" id="nav-bar">
-                    <ul class="navbar-nav mr-auto"></ul>
-                    <ul class="navbar-nav">
-                        <li class="nav-item">{!! link_to_route('signup.get','Signup', [], ['class' => 'nav-link']) !!}</li>
-                        <li class="nav-item">{!! link_to_route('','', [], ['class' => 'nav-link']) !!}</li>
-                        <li class="nav-item">{!! link_to_route('tasklists.create','タスクを作成する', [], ['class' => 'nav-link']) !!}</li>
-                    </ul>
-                </div>
-            </nav>
-        </header>
+        
+        @include('commons.navbar')
         
         <div class="container">
             @include('commons.error_tasklists')
+            
             @yield('content')
         </div>
         
